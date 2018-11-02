e5be5ed488d80f5315d3ec6e892726675da4d10b
folkelindell <fo6485ni-s@student.lu.se>
2018-11-02 15:52:03 +0100

    gui init

--- src/gui/App.java
+++ src/gui/App.java
@@ -1,14 +1,16 @@
 package gui;
 
+import controller.Controller;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 	private AppContext appContext;
+	private Controller cont;
+	private LoginPane loginPane;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -16,23 +18,38 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-
-		primaryStage.setTitle("FoJaJo Banking");
+		primaryStage.setHeight(600);
+		primaryStage.setWidth(600);
+		primaryStage.setTitle("FoJaJo Banking - Sign in");
 
 		appContext = new AppContext();
-		
-		RootPane root = new RootPane();
+		cont = new Controller();
+		loginPane = new LoginPane(cont);
+
+		RootPane rootPane = new RootPane(cont, appContext);
 		Label statusLabel = createStatusLabel();
 		
-		BorderPane appPane = new BorderPane();
-		appPane.setCenter(root);
-		appPane.setBottom(statusLabel);
-		appPane.getBottom().setId("status-bar");
-		
-		primaryStage.setScene(new Scene(appPane));
-		primaryStage.show();
-	}
+		loginPane.setBottom(statusLabel);
+		rootPane.setBottom(statusLabel);
+
 	
+		primaryStage.setScene(new Scene(loginPane));
+		primaryStage.show();
+		
+		cont.getCurrentCustomerProperty().addListener((observable, oldValue, newValue) -> {
+			if(newValue != null) {
+				primaryStage.getScene().setRoot(rootPane);
+				primaryStage.setTitle("FoJaJo Banking - " + newValue.getUsername());
+			}else {
+				primaryStage.setTitle("FoJaJo Banking - Sign in");
+				primaryStage.getScene().setRoot(loginPane);
+			}
+		});
+		primaryStage.setOnHidden(e -> {
+			if(cont.getCurrentCustomer() != null) ;
+		});
+	}
+
 	private Label createStatusLabel() {
 		Label statusLabel = new Label();
 		statusLabel.textProperty().bind(appContext.getStatusStringProp());
--- src/gui/LoginPane.java
+++ src/gui/LoginPane.java
@@ -1,11 +1,64 @@
 package gui;
 
 import controller.Controller;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
 
-public class LoginPane extends GridPane{
+public class LoginPane extends BorderPane{
 	private Controller cont;
+	
+	private Label headerLbl, accNameLbl, accPassLbl;
+	private TextField accNameInput, accPassInput;
+	private Button loginBtn, createAccBtn;
+	private GridPane contentGrid;
+	
+	{
+		componentsInit();
+		listenersInit();
+		
+	}
 	public LoginPane(Controller cont) {
 		this.cont = cont;
+	}
+	
+	private void componentsInit() {
+		contentGrid = new GridPane();
+		contentGrid.setVgap(5);
+		contentGrid.setMaxSize(150, 250);
+		
+		headerLbl = new Label("Sign in");
+		headerLbl.setFont(Font.font(30));
+		accNameLbl = new Label("Username:");
+		accPassLbl = new Label("Password:");
+		accNameInput = new TextField();
+		accPassInput = new TextField();
+		loginBtn = new Button("Sign in");
+		GridPane.setHgrow(loginBtn, Priority.ALWAYS);
+		loginBtn.setMaxWidth(Double.MAX_VALUE);
+		createAccBtn = new Button("Create account");
+		GridPane.setHgrow(createAccBtn, Priority.ALWAYS);
+		createAccBtn.setMaxWidth(Double.MAX_VALUE);
+
+		
+		contentGrid.add(headerLbl, 0, 0);
+		contentGrid.add(accNameLbl, 0, 1);
+		contentGrid.add(accNameInput, 0, 2);
+		contentGrid.add(accPassLbl, 0, 3);
+		contentGrid.add(accPassInput, 0, 4);
+		contentGrid.add(loginBtn, 0, 5);
+		contentGrid.add(createAccBtn, 0, 6);
+		
+		setCenter(contentGrid);
+	}
+	
+	private void listenersInit() {
+		loginBtn.setOnAction(e -> {
+			cont.logInUser(accNameInput.getText(), accPassInput.getText());
+		});
 	}
 }
