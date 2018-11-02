bc514b77b79f4fb1c023358c0dab846b39bb242f
jacobtornqvist <35672110+jacobtornqvist@users.noreply.github.com>
2018-11-02 11:48:47 +0100

    Update src/dal/TransactionDAO.java
    yes! just deal with it

--- src/dal/TransactionDAO.java
+++ src/dal/TransactionDAO.java
@@ -2,24 +2,10 @@ package dal;
 
 import java.sql.ResultSet;
 import model.Transaction;
-import model.TransactionLog;
 
-public class TransactionDAO {
-	
-	
+
+public class TransactionDAO {	
 	ResultSet result = null;
-	
-	
-
-
-	public TransactionLog createTransactionLog(TransactionLog transactionLog)throws Exception{
-		try {
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		return transactionLog;
-	}
 
 	public Boolean transfer(Transaction transaction)throws Exception {
 		try {
