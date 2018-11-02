2598a4ce6c401e8f34bee40754e7341be9212887
jacobtornqvist <35672110+jacobtornqvist@users.noreply.github.com>
2018-11-02 13:45:22 +0100

    New model

---
+++ src/model/LogEntry.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class LogEntry {
+	
+	private int logID; 
+	private double amount;
+	private String counterParty;
+	private Timestamp logTime;
+	private int accountNbr;
+	
+	public int getLogID() {
+		return logID;
+	}
+	public void setLogID(int logID) {
+		this.logID = logID;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getCounterParty() {
+		return counterParty;
+	}
+	public void setCounterParty(String counterParty) {
+		this.counterParty = counterParty;
+	}
+	public Timestamp getLogTime() {
+		return logTime;
+	}
+	public void setLogTime(Timestamp logTime) {
+		this.logTime = logTime;
+	}
+	public int getAccountNbr() {
+		return accountNbr;
+	}
+	public void setAccountNbr(int accountNbr) {
+		this.accountNbr = accountNbr;
+	}
+	
+
+
+
+}
