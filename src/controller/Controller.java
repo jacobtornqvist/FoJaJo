9f8fe0a3c664fffe6f72f2a1283674c6df051121
folkelindell <fo6485ni-s@student.lu.se>
2018-11-05 18:10:30 +0100

    gui början

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,13 +1,20 @@
 package controller;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import model.BankAccount;
 import model.Customer;
+import model.LogEntry;
 
 public class Controller {
 	private ObjectProperty<Customer> currentCustomerProperty;
+	private ObjectProperty<BankAccount> currentBankAccountProperty;
 	{
 		currentCustomerProperty = new SimpleObjectProperty<Customer>();
+		currentBankAccountProperty = new SimpleObjectProperty<BankAccount>();
 	}
 
 	public void logInUser(String accName, String accPass) {
@@ -22,4 +29,31 @@ public class Controller {
 		return currentCustomerProperty.get();
 	}
 
+	public ArrayList<BankAccount> getBankAccounts(Customer c) {
+		ArrayList<BankAccount> temp = new ArrayList<BankAccount>();
+		temp.add(new BankAccount(1, "tempKontoNamn", "asf" , 23));
+		return temp;
+	}
+
+	public void setCurrentCustomer(Customer c) {
+		currentCustomerProperty.set(c);
+	}
+
+	public ObjectProperty<BankAccount> getCurrentBankAccountProperty() {
+		return currentBankAccountProperty;
+	}
+
+	public void setCurrentBankAccount(BankAccount b) {
+		currentBankAccountProperty.set(b);
+	}
+	
+	public BankAccount getCurrentBankAccount() {
+		return currentBankAccountProperty.get();
+	}
+
+	public ArrayList<LogEntry> getLogEntries(BankAccount b) {
+		ArrayList<LogEntry> temp = new ArrayList<LogEntry>();
+		temp.add(new LogEntry(1, 23, "Pelle", new Timestamp(System.currentTimeMillis()), 30));
+		return temp;
+	}
 }
--- src/gui/RootPane.java
+++ src/gui/RootPane.java
@@ -6,8 +6,24 @@ import javafx.scene.layout.BorderPane;
 public class RootPane extends BorderPane {
 	
 	private AppContext appContext;
+	private CustomerPane custPane;
+	private BankAccountPane bankAccountPane;
+	
 	public RootPane(Controller cont, AppContext appContext) {
 		setId("root-pane");
+		this.appContext = appContext;
+		custPane = new CustomerPane(cont, appContext);
+		bankAccountPane = new BankAccountPane(cont, appContext);
 		
+		setCenter(custPane);
+		
+		cont.getCurrentBankAccountProperty().addListener((observable, oldValue, newValue) -> {
+			if(newValue != null) {
+				setCenter(bankAccountPane);
+			}else {
+				setCenter(custPane);
+			}
+		});
 	}
+	
 }
