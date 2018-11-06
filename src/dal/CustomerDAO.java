ab7227de1ad83da05c38af4f9c2f3bff5b27ac67
jacobtornqvist <35672110+jacobtornqvist@users.noreply.github.com>
2018-11-06 22:44:08 +0100

    update

--- src/dal/CustomerDAO.java
+++ src/dal/CustomerDAO.java
@@ -3,7 +3,6 @@ package dal;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import model.Customer;
